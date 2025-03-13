af2db5ac125d80c85a053dba9ef4d44270e6478d
Onelots <onelots@onelots.fr>
2025-03-13 01:27:19 +0100

    b1c1: Rebrand as Caelum kernel
    Signed-off-by: Onelots <onelots@onelots.fr>

--- anykernel.sh
+++ anykernel.sh
@@ -4,7 +4,7 @@
 ### AnyKernel setup
 # global properties
 properties() { '
-kernel.string=Kernel 4.9.337 for Blueline & Crosshatch by Onelots
+kernel.string=Caelum Kernel 4.9.337 for Blueline & Crosshatch by Onelots | https://discord.onelots.fr
 do.devicecheck=1
 do.modules=0
 do.systemless=1
