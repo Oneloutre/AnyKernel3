ede933ee20ed930fd87255e0ea318902f88a9cfa
Onelots <onelots@onelots.fr>
2025-03-13 02:27:42 +0100

    b4s4: Rebrand as Caelum kernel
    Signed-off-by: Onelots <onelots@onelots.fr>

--- anykernel.sh
+++ anykernel.sh
@@ -4,7 +4,7 @@
 ### AnyKernel setup
 # global properties
 properties() { '
-kernel.string=Kernel 4.9.337 for Bonito & Sargo by Onelots
+kernel.string=Caelum Kernel 4.9.337 for Bonito & Sargo by Onelots | https://discord.onelots.fr
 do.devicecheck=1
 do.modules=0
 do.systemless=1
